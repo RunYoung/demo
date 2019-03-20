c718857f4616f0a2bf6c9ce7b2ea3b008d7b149a
RunYoung <yangyuan@163.com>
2019-03-20 09:43:48 +0800

    no message

---
+++ testd/src/main/test/Main.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * Created by yangyuan on 2019/3/19.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("testing....");
+    }
+}
+
+
